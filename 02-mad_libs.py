59aa6331c139abad88f8a582855ca5c8041ef61d
Adam Cherochak <genericCog@users.noreply.github.com>
2016-11-01 14:29:07 -0700

    Complete code for project

---
+++ 02-mad_libs.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+adjective2 = input("Enter another adjective: ")
+
+
+print(("Roses are"),adjective1)
+print(noun1, ("are blue"))
+print(noun2, ("is"), adjective2)
+print ("And so are you.")
